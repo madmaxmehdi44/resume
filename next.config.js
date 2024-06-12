727e681796bb966b253e2ab302d22ba6c287b112
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2024-06-12 07:22:16 +0330

    change env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        RESEND_API : process.env.RESEND_API,
+        NEXT_PUBLIC_RESEND_API : process.env.NEXT_PUBLIC_RESEND_API,
     },
 }
 
